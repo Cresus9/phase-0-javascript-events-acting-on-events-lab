6e478251c49cff639397b1b74965a15c9c2e7ec6
Sigui Thierry Herman Yabre <thierry.yabre@yahoo.fr>
2022-06-16 11:54:46 -0400

    commit tree f65999001a0c6b54cdfa687f00513c2e774a402a

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Your code here
+/*const dogder = document.getElementById('dodger');
+dogder.style.backgroundColor = "#FF69B4";
+dogder.style.bottom = '0px';
+dogder.style.left = 'opx';
+
+document.addEventListener("keydown", function (event){
+    console.log(event);
+});
+*/
+const dodger = document.getElementById("dodger");
+
+document.addEventListener("keydown", function(e){
+    if (e.key === "ArrowLeft"){
+        moveDodgerLeft();
+    }
+});
+
+function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace("px", "")
+        const left = parseInt(leftNumbers, 10);
+
+        if (left>0){
+            dodger.style.left = `${left - 1}`;
+        }
+}
+
+
+function moveDodgerRight(){
+    const rightNumbers = dodger.style.left.replace("px", "")
+    const right = parseInt(rightNumbers, 10);
+
+    if (right<0){
+        dodger.style.left = `${left + 1}`;
+    }
+}
+document.addEventListener("keydow", function (e){
+    if (e.key === "ArrowRight"){
+        moveDodgerRight
+    }
+});
